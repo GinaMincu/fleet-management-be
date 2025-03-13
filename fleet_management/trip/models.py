6603caa3633dcbf1d1fd3e71da5eddfb130c8b92
GinaMincu <mincugina72@yahoo.com>
2025-03-13 18:10:02 +0200

    start_ts/end_ts from date to datetime

--- fleet_management/trip/models.py
+++ fleet_management/trip/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-import datetime
+from datetime import datetime
 from fleet_management.vehicle.models import Vehicle
 
 class Trip(models.Model):
   vehicle = models.ForeignKey(
         Vehicle, related_name='trips', on_delete=models.CASCADE,null=True)
   driver=models.CharField(max_length=40,default="")
-  start_ts=models.DateField(blank=True, null=True,default=datetime.date.today)
-  end_ts=models.DateField(blank=True, null=True,default=datetime.date.today)
+  start_ts=models.DateTimeField(blank=True, null=True,default=datetime.now)
+  end_ts=models.DateTimeField(blank=True, null=True,default=datetime.now)
 
   def __str__(self):
         return self.driver+ "-" + self.vehicle.plate
